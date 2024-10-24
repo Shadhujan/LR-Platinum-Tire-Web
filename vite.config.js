e272c3e4cce48c4b4ad37d792c979dd0c95951fd
Shadhujan <145929070+Shadhujan@users.noreply.github.com>
2024-10-24 23:35:49 +0530

    updated edit,admin

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['firebase/compat/app', 'firebase/compat/storage'],
-    include: ['@iconify/react']
+    include: ['firebase/compat/app', 'firebase/compat/storage','@iconify/react']
   }
 })
